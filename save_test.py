453a05155fd9f887c1914980e943a8089e945c21
Mike <mikebuntu68@gmail.com>
2021-12-13 19:53:18 -0500

    Remove non zdspy library-related files

--- save_test.py
+++
@@ -1,9 +0,0 @@
-from zdspy import dataio as d, zmb
-
-og_file = d.ReadFile("./isle_main_00.zmb")
-
-_zmb = zmb.ZMB(og_file)
-
-save_zmb = _zmb.save()
-
-print("Same file: " + str(og_file == save_zmb))
